83434dc81fc6bd738b11e5b0ff6c7b8fe20160fe
twocucao <twocucao@gmail.com>
2021-03-02 19:02:58 +0800

    add tailwind.config

--- src/pages/Monitor/PerformanceRequest/SQLCodeBlock.tsx
+++ src/pages/Monitor/PerformanceRequest/SQLCodeBlock.tsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/github'
 import { format } from 'sql-formatter'
 
-const Pre = styled.pre`
-  text-align: left;
-  margin: 1em 0;
-  padding: 0.5em;
-  overflow: scroll;
-`
-
-const Line = styled.div`
-  display: table-row;
-`
-
-const LineNo = styled.span`
-  display: table-cell;
-  text-align: right;
-  padding-right: 1em;
-  user-select: none;
-  opacity: 0.5;
-`
-
-const LineContent = styled.span`
-  display: table-cell;
-`
+// const Pre = styled.pre`
+//   text-align: left;
+//   margin: 1em 0;
+//   padding: 0.5em;
+//   overflow: scroll;
+// `
+//
+// const Line = styled.div`
+//   display: table-row;
+// `
+//
+// const LineNo = styled.span`
+//   display: table-cell;
+//   text-align: right;
+//   padding-right: 1em;
+//   user-select: none;
+//   opacity: 0.5;
+// `
+//
+// const LineContent = styled.span`
+//   display: table-cell;
+// `
 
 function shortenSQLSelect(line: string) {
   if (line.startsWith('SELECT ')) {
@@ -37,20 +37,9 @@ function shortenSQLSelect(line: string) {
 export const SQLCodeBlock = ({ sql }: { sql: string }) => {
   const formattedSQL = format(sql, { indent: '\xa0\xa0\xa0\xa0' })
   return (
-    <Highlight {...defaultProps} theme={theme} code={formattedSQL} language="sql">
+    <Highlight {...defaultProps} theme={theme} code={formattedSQL} language='sql'>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <Line key={i} {...getLineProps({ line, key: i })}>
-              <LineNo>{i + 1}</LineNo>
-              <LineContent>
-                {line.map((token, key) => {
-                  return <span key={key} {...getTokenProps({ token, key })} />
-                })}
-              </LineContent>
-            </Line>
-          ))}
-        </Pre>
+        <div></div>
       )}
     </Highlight>
   )
@@ -59,20 +48,9 @@ export const SQLCodeBlock = ({ sql }: { sql: string }) => {
 export const SQLCodeBlockOneLiner = ({ sql }: { sql: string }) => {
   const oneLiner = shortenSQLSelect(sql.replaceAll('\n', ''))
   return (
-    <Highlight {...defaultProps} theme={theme} code={oneLiner} language="sql">
+    <Highlight {...defaultProps} theme={theme} code={oneLiner} language='sql'>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <Line key={i} {...getLineProps({ line, key: i })}>
-              <LineNo>{i + 1}</LineNo>
-              <LineContent>
-                {line.map((token, key) => {
-                  return <span key={key} {...getTokenProps({ token, key })} />
-                })}
-              </LineContent>
-            </Line>
-          ))}
-        </Pre>
+        <div></div>
       )}
     </Highlight>
   )
@@ -82,18 +60,7 @@ export const CodeBlock = ({ snippet, language }: { snippet: string; language: La
   return (
     <Highlight {...defaultProps} theme={theme} code={snippet} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <Line key={i} {...getLineProps({ line, key: i })}>
-              <LineNo>{i + 1}</LineNo>
-              <LineContent>
-                {line.map((token, key) => {
-                  return <span key={key} {...getTokenProps({ token, key })} />
-                })}
-              </LineContent>
-            </Line>
-          ))}
-        </Pre>
+        <div></div>
       )}
     </Highlight>
   )
