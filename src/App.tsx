7f9696b230302281f8df2ecbf40321c7a1d69674
twocucao <twocucao@gmail.com>
2022-03-24 22:12:57 +0800

    remove-graphql-related-tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import "./App.css"
-import {ProvideStore, useGlobalStore} from "@/hooks/useStore"
+import { ProvideStore, useGlobalStore } from "@/hooks/useStore"
 import Router from "@/Router"
-import {PageLoading} from "@ant-design/pro-layout"
+import { PageLoading } from "@ant-design/pro-layout"
 
 function Main() {
-    const store = useGlobalStore()
-    // return <PageLoading />
-    return <Router/>
+  const store = useGlobalStore()
+  // return <PageLoading />
+  return <Router />
 }
 
 export const App: React.FC = (props: { children?: React.ReactNode }) => {
-    return (
-        <ProvideStore>
-            <Main/>
-        </ProvideStore>
-    )
+  return (
+    <ProvideStore>
+      <Main />
+    </ProvideStore>
+  )
 }
 
 export default App
